a86da3ba1f1042785d0d9c2962321085523e4152
Brandon Boch López <alejandrobochlopez@gmail.com>
2025-04-25 23:13:33 -0600

    Ingreso de datos a reclutamiento y selección

--- Codigo/Modulos/SGRRHH/ReclutamientoSeleccion/Reclutamiento/Capa_Vista_Reclutamiento/Frm_ats.cs
+++ Codigo/Modulos/SGRRHH/ReclutamientoSeleccion/Reclutamiento/Capa_Vista_Reclutamiento/Frm_ats.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Capa_Controlador_Reclutamiento;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -25,7 +26,7 @@ namespace Capa_Vista_Reclutamiento
             navegador1.AsignarColorFondo(ColorTranslator.FromHtml("#B4D2F0"));
             navegador1.AsignarColorFuente(Color.Black);
             navegador1.AsignarTabla("Tbl_ats");
-            navegador1.ObtenerIdAplicacion("13004");
+            navegador1.ObtenerIdAplicacion("1000");
             navegador1.ObtenerIdUsuario(idUsuario);
             navegador1.AsignarAyuda("1");
             navegador1.AsignarNombreForm("");
