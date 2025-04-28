212e75e57a0916bce874d4a317f7a25a2414d9d0
JdanielS7 <jsierrac3@miumg.edu,gt>
2025-04-27 21:49:45 -0600

    Correcciones evalacion y desempenio

--- Codigo/Modulos/SGRRHH/EvaluacionDesempenio/Capa_Vista_Evaluacion/Frm_Resultados_Evaluacion.cs
+++ Codigo/Modulos/SGRRHH/EvaluacionDesempenio/Capa_Vista_Evaluacion/Frm_Resultados_Evaluacion.cs
@@ -18,7 +18,13 @@ namespace Capa_Vista_Evaluacion
 
             string idUsuario = Interfac_V3.UsuarioSesion.GetIdUsuario();
 
-            string[] alias = { "ID Resultado", "ID Empleado", "Calificación Final", "Fecha Consolidación", "Estado" };
+            string[] alias = { "ID Evaluación",
+        "ID Empleado", "ID Evaluador",
+        "Tipo Evaluación",
+        "Calificación",
+        "Comentarios",
+        "Fecha Evaluación",
+        "Estado"  };
             navegador1.AsignarAlias(alias);
             navegador1.AsignarSalida(this);
             navegador1.AsignarColorFondo(ColorTranslator.FromHtml("#B4D2F0"));
@@ -26,7 +32,7 @@ namespace Capa_Vista_Evaluacion
             navegador1.ObtenerIdAplicacion("17001");
             navegador1.AsignarAyuda("1");
             navegador1.ObtenerIdUsuario(idUsuario);
-            navegador1.AsignarTabla("tbl_resultados_evaluacion");
+            navegador1.AsignarTabla("tbl_evaluaciones");
 
             navegador1.AsignarNombreForm("RESULTADOS DE EVALUACIÓN");
             
