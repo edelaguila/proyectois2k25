59103ec3aa9afb55a11dc19a34248822c46da378
Salvador <rmartinezo@miumg.edu.gt>
2025-05-08 10:36:02 -0600

    mas cambios

--- Codigo/Modulos/Comercial2_5/MVC_Compras/Capa_Modelo_Compras/Sentencias.cs
+++ Codigo/Modulos/Comercial2_5/MVC_Compras/Capa_Modelo_Compras/Sentencias.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Odbc;
@@ -70,19 +71,56 @@ namespace Capa_Modelo_Compras
             return sucursales;
         }
 
+        public List<string> ObtenerSucursal()
+        {
+            List<string> sucursales = new List<string>();
+            OdbcConnection connection = conn.conexion();
 
-        public void InsertarCompra(int proveedor ,DateTime fechaCompra,string factura,string compro, string pago, double sub , double imp , double total ,string prod, double cant ,double pre , string desc)
+            try
+            {
+                string query = "SELECT Pk_ID_BODEGA FROM tbl_bodegas";
+                OdbcCommand cmd = new OdbcCommand(query, connection);
+                OdbcDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    sucursales.Add(reader.GetString(0));
+                }
+
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al obtener sucursales: " + ex.Message);
+            }
+            finally
+            {
+                conn.desconexion(connection);
+            }
+
+            return sucursales;
+        }
+
+
+
+
+
+
+        public void InsertarCompra(int proveedor ,DateTime fechaCompra,int bod, string factura,string compro, string pago, double sub , double imp , double total ,string prod, double cant ,double pre , string desc)
         {
             OdbcConnection o_conn = conn.conexion();
             try
             {
                 // SQL para insertar solo fecha_compra
-                string s_query = "INSERT INTO Tbl_compra (Fk_prov_id ,fecha_compra,numero_factura,tipo_comprobante,forma_pago,subtotal, impuestos, total ,producto,cantidad,precio,descripcion) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)";
+                string s_query = "INSERT INTO Tbl_compra (Fk_prov_id ,fecha_compra,Fk_ID_BODEGA ,numero_factura,tipo_comprobante,forma_pago,subtotal, impuestos, total ,producto,cantidad,precio,descripcion) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)";
                 OdbcCommand cmd = new OdbcCommand(s_query, o_conn);
 
                 // Agregar el parámetro 'fecha_compra'
                 cmd.Parameters.AddWithValue("@Fk_prov_id", proveedor);
                 cmd.Parameters.AddWithValue("@fecha_compra", fechaCompra);
+                cmd.Parameters.AddWithValue("@Fk_ID_BODEGA", bod);
+
+                
                 cmd.Parameters.AddWithValue("@numero_factura", factura);
                 cmd.Parameters.AddWithValue("@tipo_comprobante", compro);
                 cmd.Parameters.AddWithValue("@forma_pago", pago);
@@ -104,7 +142,30 @@ namespace Capa_Modelo_Compras
 
 
 
-                // cmd.Parameters.AddWithValue("@subtotal", subtotal);
+                string s_query2 = "SELECT  Pk_id_Producto FROM Tbl_productos WHERE nombreProducto = ?";
+                OdbcCommand cmd2 = new OdbcCommand(s_query2, o_conn);
+
+                cmd2.Parameters.AddWithValue("@producto", prod);
+
+                OdbcDataReader reader = cmd2.ExecuteReader();
+
+                int idProducto = -1; // Variable para guardar el valor (ajusta el tipo si es string, etc.)
+
+                if (reader.Read())
+                {
+                    idProducto = reader.GetInt32(0); // 0 es el índice de la columna en la consulta
+                }
+
+                reader.Close(); // Cierra el lector después de usarlo
+
+
+
+               /* // SQL para insertar solo fecha_compra
+                string s_query3 = "INSERT INTO Tbl_compra (Fk_prov_id ,fecha_compra,numero_factura,tipo_comprobante,forma_pago,subtotal, impuestos, total ,producto,cantidad,precio,descripcion) VALUES (?,?,?,?,?)";
+                OdbcCommand cmd3 = new OdbcCommand(s_query3, o_conn);
+
+                // Agregar el parámetro 'fecha_compra'
+                cmd3.Parameters.AddWithValue("@Fk_prov_id", proveedor);*/
 
 
 
--- Codigo/Modulos/Comercial2_5/MVC_Compras/ClassLibrary1/Controlador.cs
+++ Codigo/Modulos/Comercial2_5/MVC_Compras/ClassLibrary1/Controlador.cs
@@ -21,11 +21,18 @@ namespace Capa_Controlador_Compras
         }
 
 
+        public List<string> ObtenerSucursales2()
+        {
+            return _sentencias.ObtenerSucursal();
+        }
 
-        public void Pro_RegistrarCompra(int proveedor, DateTime fechaCompra, string factura, string compro, string pago, double sub, double imp, double total, string prod, double cant, double pre, string desc)
+
+
+
+        public void Pro_RegistrarCompra(int proveedor, DateTime fechaCompra, int bod, string factura, string compro, string pago, double sub, double imp, double total, string prod, double cant, double pre, string desc)
         {
             // Llamada al método InsertarCompra con los parámetros que recibe el controlador
-            _sentencias.InsertarCompra(proveedor, fechaCompra, factura, compro, pago,sub , imp, total, prod,cant , pre,desc);
+            _sentencias.InsertarCompra(proveedor, fechaCompra, bod, factura, compro, pago,sub , imp, total, prod,cant , pre,desc);
         }
         public DataTable Fun_MostrarMovimientosInventario()
         {
