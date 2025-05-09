be761d10cdb4f79502697f29d73781efbdef3d0c
Javier-cyber-Wx <francisco12102002cl@gmail.com>
2025-05-09 08:28:53 -0600

    se agregaron los nuevos formularios

---
+++ Codigo/Portal Web/Portal Web Para Empleados/Portal Web/Models/Asistencia.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Portal_Web.Models
+{
+    [Table("tbl_asistencias")]
+    public class Asistencia
+    {
+        [Key]
+        [Column("pk_id_asistencia")]
+        public int Id { get; set; }
+
+        [Column("fecha")]
+        public DateTime fecha { get; set; }
+
+        [Column("hora_entrada")]
+        public TimeSpan? hora_entrada { get; set; }
+
+        [Column("hora_salida")]
+        public TimeSpan? hora_salida { get; set; }
+
+        [Column("estado_asistencia")]
+        public string estado_asistencia { get; set; }
+
+        [Column("observaciones")]
+        public string observaciones { get; set; }
+
+        [Column("fk_clave_empleado")]
+        public int fk_clave_empleado { get; set; }
+
+        [ForeignKey("fk_clave_empleado")]
+        public virtual Empleado Empleado { get; set; }
+    }
+}
---
+++ Codigo/Portal Web/Portal Web Para Empleados/Portal Web/Models/Vacaciones.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Portal_Web.Models
+{
+    [Table("tbl_asignacion_vacaciones")]
+    public class Asignacion_Vacaciones
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int pk_registro_vaciones { get; set; }
+
+        [StringLength(25)]
+        public string asignacion_vacaciones_descripcion { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime asignacion_vacaciones_fecha_inicio { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime asignacion_vacaciones_fecha_fin { get; set; }
+
+        [ForeignKey("Empleado")]
+        public int fk_clave_empleado { get; set; }
+
+        public bool estado { get; set; }
+
+        // Relación con empleado
+        public virtual Empleado Empleado { get; set; }
+    }
+}
