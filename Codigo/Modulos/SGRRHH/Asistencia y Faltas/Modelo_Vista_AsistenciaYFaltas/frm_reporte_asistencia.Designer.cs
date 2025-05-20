f8a7709cf5f3256388e51b10e95955dd821ec85d
Marco Alejandro Monroy Rousselin <mmonroyr3@miumg.edu.gt>
2025-05-19 20:30:02 -0600

    Subida Final

--- Codigo/Modulos/SGRRHH/Asistencia y Faltas/Modelo_Vista_AsistenciaYFaltas/frm_reporte_asistencia.Designer.cs
+++ Codigo/Modulos/SGRRHH/Asistencia y Faltas/Modelo_Vista_AsistenciaYFaltas/frm_reporte_asistencia.Designer.cs
@@ -29,8 +29,14 @@ namespace Modelo_Vista_AsistenciaYFaltas
         /// </summary>
         private void InitializeComponent()
         {
-            this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
             this.ReposrteAsistencia1 = new Modelo_Vista_AsistenciaYFaltas.ReposrteAsistencia();
+            this.AsisRep2 = new Modelo_Vista_AsistenciaYFaltas.AsisRep();
+            this.AsisRep1 = new Modelo_Vista_AsistenciaYFaltas.AsisRep();
+            this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.AsistenciReporte1 = new Modelo_Vista_AsistenciaYFaltas.AsistenciReporte();
+            this.AsistenciReporte2 = new Modelo_Vista_AsistenciaYFaltas.AsistenciReporte();
+            this.crystalReportViewer2 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.AsistenciReporte3 = new Modelo_Vista_AsistenciaYFaltas.AsistenciReporte();
             this.SuspendLayout();
             // 
             // crystalReportViewer1
@@ -41,15 +47,28 @@ namespace Modelo_Vista_AsistenciaYFaltas
             this.crystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.crystalReportViewer1.Location = new System.Drawing.Point(0, 0);
             this.crystalReportViewer1.Name = "crystalReportViewer1";
-            this.crystalReportViewer1.ReportSource = this.ReposrteAsistencia1;
+            this.crystalReportViewer1.ReportSource = this.AsistenciReporte2;
             this.crystalReportViewer1.Size = new System.Drawing.Size(800, 450);
             this.crystalReportViewer1.TabIndex = 0;
+            // 
+            // crystalReportViewer2
+            // 
+            this.crystalReportViewer2.ActiveViewIndex = 0;
+            this.crystalReportViewer2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.crystalReportViewer2.Location = new System.Drawing.Point(0, 0);
+            this.crystalReportViewer2.Name = "crystalReportViewer2";
+            this.crystalReportViewer2.ReportSource = this.AsistenciReporte3;
+            this.crystalReportViewer2.Size = new System.Drawing.Size(800, 450);
+            this.crystalReportViewer2.TabIndex = 1;
             // 
             // frm_reporte_asistencia
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.crystalReportViewer2);
             this.Controls.Add(this.crystalReportViewer1);
             this.Name = "frm_reporte_asistencia";
             this.Text = "frm_reporte_asistencia";
@@ -61,5 +80,11 @@ namespace Modelo_Vista_AsistenciaYFaltas
 
         private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
         private ReposrteAsistencia ReposrteAsistencia1;
+        private AsisRep AsisRep1;
+        private AsisRep AsisRep2;
+        private AsistenciReporte AsistenciReporte1;
+        private AsistenciReporte AsistenciReporte2;
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer2;
+        private AsistenciReporte AsistenciReporte3;
     }
 }
